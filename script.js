e4cc23ecbd3e6a66e13ccdcdc6d984d8afa85f38
Rafael <you@example.com>
2023-06-10 19:32:07 -0300

    media changes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function toggleMode() {
     // substituir a imagem 
     if (html.classList.contains('light')) {
         //se tiver light mode, adicionar a imagem light
-        img.setAttribute('src', './assets/avatar-light.png')
+        img.setAttribute('src', './assets/avatar-light.jpeg')
     } else {
         // se tiver sem light mode, manter a foto normal
         img.setAttribute('src', './assets/avatar.png')
